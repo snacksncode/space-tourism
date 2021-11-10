1558287f4348ea3f3b17abd9b0b524dd02a70455
snacksncode <snacksncode@gmail.com>
2021-11-10 13:29:25 +0100

    feat: added all interactive elements and styling

--- pages/design-system.tsx
+++ pages/design-system.tsx
@@ -150,6 +150,159 @@ const DesignSystem: NextPage = () => {
             </div>
           </div>
         </section>
+
+        <section
+          className="flow"
+          style={{ paddingBottom: "2rem", "--flow-spacing": "4rem" } as any}
+          id="interactive-elements"
+        >
+          <h2 className="numbered-title">
+            <span>03</span> Interactive elements
+          </h2>
+
+          {/* navigation */}
+          <div style={{ marginTop: "2rem" }}>
+            <nav style={{ backgroundColor: "rgba(255,255,255, 0.04)" }}>
+              <ul
+                className="primary-navigation ff-sans-cond flex underline-indicators"
+                style={{ justifyContent: "center" }}
+              >
+                <li className="active uppercase text-white">
+                  <a className="flex letter-spacing-md" href="#">
+                    <span>01</span>
+                    Active
+                  </a>
+                </li>
+                <li className="uppercase text-white" data-hovered>
+                  <a className="flex letter-spacing-md" href="#">
+                    <span>02</span>
+                    Hovered
+                    <div className="fake-pointer" style={{ bottom: 20, right: 5 }}>
+                      <Image src="/assets/pointer.svg" width={30} height={30} alt="fake pointer" />
+                    </div>
+                  </a>
+                </li>
+                <li className="uppercase text-white">
+                  <a className="flex letter-spacing-md" href="#">
+                    <span>03</span>
+                    Idle
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <p style={{ marginTop: "1rem", textAlign: "center" }} className="text-accent ff-sans fs-400">
+              Different States of Navigation Bar
+            </p>
+          </div>
+
+          <div className="flex" style={{ alignItems: "center", justifyContent: "space-around" }}>
+            <div className="flow" style={{ "--flow-spacing": "6rem" } as any}>
+              {/* explore button */}
+              <div>
+                <a href="#" className="large-button grid center uppercase ff-serif fs-600 bg-white text-dark">
+                  Explore
+                </a>
+                <p style={{ marginTop: "1rem" }} className="text-accent ff-sans fs-400">
+                  Landing Page Main Button - Idle
+                </p>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  data-hovered
+                  className="large-button grid center uppercase ff-serif fs-600 bg-white text-dark"
+                >
+                  Explore
+                  <div className="fake-pointer" style={{ bottom: 50, right: 30 }}>
+                    <Image src="/assets/pointer.svg" width={30} height={30} alt="fake pointer" />
+                  </div>
+                </a>
+                <p style={{ marginTop: "5rem" }} className="text-accent ff-sans fs-400">
+                  Landing Page Main Button - Hover
+                </p>
+              </div>
+            </div>
+
+            <div className="flow" style={{ "--flow-spacing": "6rem" } as any}>
+              {/* Tabs */}
+              <div className="flex" style={{ flexDirection: "column" }}>
+                <div role="tablist" className="tab-list flex underline-indicators" style={{ justifyContent: "center" }}>
+                  <button
+                    role="tab"
+                    aria-selected="true"
+                    className="uppercase text-accent letter-spacing-md ff-sans-cond"
+                  >
+                    Moon
+                  </button>
+                  <button
+                    role="tab"
+                    aria-selected="false"
+                    data-hovered
+                    className="uppercase text-accent letter-spacing-md ff-sans-cond"
+                  >
+                    Mars
+                    <div className="fake-pointer">
+                      <Image src="/assets/pointer.svg" width={30} height={30} alt="fake pointer" />
+                    </div>
+                  </button>
+                  <button
+                    role="tab"
+                    aria-selected="false"
+                    className="uppercase text-accent letter-spacing-md ff-sans-cond"
+                  >
+                    Europa
+                  </button>
+                </div>
+                <p style={{ textAlign: "center" }} className="text-accent ff-sans fs-400">
+                  Tabs (Active, Hover, & Idle)
+                </p>
+              </div>
+
+              {/* Dots */}
+              <div className="flex" style={{ flexDirection: "column" }}>
+                <div role="tablist" className="dot-indicators flex" style={{ justifyContent: "center" }}>
+                  <button role="tab" aria-selected="true">
+                    <span className="sr-only">Slide title</span>
+                  </button>
+                  <button role="tab" data-hovered aria-selected="false">
+                    <span className="sr-only">Slide title</span>
+                    <div className="fake-pointer" style={{ bottom: -15, right: -15 }}>
+                      <Image src="/assets/pointer.svg" width={30} height={30} alt="fake pointer" />
+                    </div>
+                  </button>
+                  <button role="tab" aria-selected="false">
+                    <span className="sr-only">Slide title</span>
+                  </button>
+                </div>
+                <p style={{ textAlign: "center" }} className="text-accent ff-sans fs-400">
+                  Slider 1 States (Active, Hover, & Idle)
+                </p>
+              </div>
+
+              {/* Numbers */}
+              <div className="flex" style={{ flexDirection: "column" }}>
+                <div role="tablist" className="number-indicators ff-serif flex" style={{ alignItems: "center" }}>
+                  <button role="tab" aria-selected="true">
+                    1
+                  </button>
+                  <button role="tab" data-hovered aria-selected="false">
+                    2
+                    <div className="fake-pointer" style={{ bottom: 0, right: 0 }}>
+                      <Image src="/assets/pointer.svg" width={30} height={30} alt="fake pointer" />
+                    </div>
+                  </button>
+                  <button role="tab" aria-selected="false">
+                    3
+                  </button>
+                </div>
+                <p style={{ textAlign: "center" }} className="text-accent ff-sans fs-400">
+                  Slider 2 States (Active, Hover, & Idle)
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
